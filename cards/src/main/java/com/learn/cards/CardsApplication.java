7cfd053f57ebcc7506af4acc420cfd09a0622388
knpragash <knpragash@gmail.com>
2023-09-26 21:26:15 +0800

    Initial

---
+++ cards/src/main/java/com/learn/cards/CardsApplication.java
@@ -0,0 +1,15 @@
+package com.learn.cards;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardsApplication.class, args);
+
+		System.out.println("Bank - Cards Started");
+	}
+
+}
---
+++ cards/src/main/java/com/learn/cards/entity/Cards.java
@@ -0,0 +1,36 @@
+package com.learn.cards.entity;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Cards extends BaseEntity {
+
+	@Id
+	@GeneratedValue(strategy= GenerationType.AUTO,generator="native")
+	@GenericGenerator(name = "native",strategy = "native")
+	private Long cardId;
+
+	private String mobileNumber;
+
+	private String cardNumber;
+
+	private String cardType;
+
+	private int totalLimit;
+
+	private int amountUsed;
+
+	private int availableAmount;
+	
+}
